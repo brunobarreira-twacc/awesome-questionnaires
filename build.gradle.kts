ac32d1a9108d879a393001d1cbde87c3d5b8920d
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-07-21 13:50:12 -0300

    chore: add Mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -9,9 +10,22 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass.set("com.awesomequestionnaires.Main")
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.mockito:mockito-junit-jupiter:5.23.0")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
