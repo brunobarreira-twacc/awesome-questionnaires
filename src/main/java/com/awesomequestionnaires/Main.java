36a83b6020ff3f92f49e44871da8c785209cd39f
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-07-22 17:23:20 -0300

    feat: add initial menu for cli

--- src/main/java/com/awesomequestionnaires/Main.java
+++ src/main/java/com/awesomequestionnaires/Main.java
@@ -1,17 +1,43 @@
 package com.awesomequestionnaires;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+            System.out.printf("====================================\nSaudacao do Questionarios Incriveis.\n====================================\n");
+            String menuPrincipal = "Digite 0 para criar um novo questionario\n" + //
+                            "Digite 1 para gerenciar questionarios\n" + //
+                            "Digite 2 para responder questionarios\n" + //
+                            "Digite 3 para sair\n\n";
+            System.out.println(menuPrincipal);
+            System.out.println("Por favor, digite sua opcao");
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 0:
+                    System.out.println("Opção não implementada, por favor, tente novamente");
+                    System.out.println(menuPrincipal);
+                    break;
+                case 1:
+                    System.out.println("Opção não implementada, por favor, tente novamente");
+                    System.out.println(menuPrincipal);
+                    break;
+                case 2:
+                    System.out.println("Opção não implementada, por favor, tente novamente");
+                    System.out.println(menuPrincipal);
+                    break;
+                case 3:
+                    System.out.printf("""
+                    ==========================================
+                    Obrigado por usar Questionarios Incriveis!
+                    ==========================================
+                    """);
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Opção não suportada, por favor, selecione uma opção do menu principal.\n");
+                    System.out.println(menuPrincipal);
+                    break;
+            }
     }
-}
+} 
