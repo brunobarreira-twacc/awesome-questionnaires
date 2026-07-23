30069c54411a66b0d67440ee2c09b7e4b5a7f94c
renatarsilvatw <renata.silva@thoughtworks.com>
2026-07-23 15:11:48 -0300

    feat: update validation inputs

--- src/main/java/com/awesomequestionnaires/Main.java
+++ src/main/java/com/awesomequestionnaires/Main.java
@@ -1,10 +1,15 @@
 package com.awesomequestionnaires;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean loopingMenu = true;
+
+        while (loopingMenu) {
+
             System.out.printf("====================================\nSaudacao do Questionarios Incriveis.\n====================================\n");
             String menuPrincipal = "Digite 0 para criar um novo questionario\n" + //
                             "Digite 1 para gerenciar questionarios\n" + //
@@ -12,32 +17,41 @@ public class Main {
                             "Digite 3 para sair\n\n";
             System.out.println(menuPrincipal);
             System.out.println("Por favor, digite sua opcao");
-            int opcao = sc.nextInt();
-            switch (opcao) {
-                case 0:
-                    System.out.println("Opção não implementada, por favor, tente novamente");
-                    System.out.println(menuPrincipal);
-                    break;
-                case 1:
-                    System.out.println("Opção não implementada, por favor, tente novamente");
-                    System.out.println(menuPrincipal);
-                    break;
-                case 2:
-                    System.out.println("Opção não implementada, por favor, tente novamente");
-                    System.out.println(menuPrincipal);
-                    break;
-                case 3:
-                    System.out.printf("""
-                    ==========================================
-                    Obrigado por usar Questionarios Incriveis!
-                    ==========================================
-                    """);
-                    sc.close();
-                    break;
-                default:
-                    System.out.println("Opção não suportada, por favor, selecione uma opção do menu principal.\n");
-                    System.out.println(menuPrincipal);
-                    break;
+            String opcao = sc.nextLine();
+
+            Pattern padraoValidacaoInput = Pattern.compile("[0-3]");
+
+            Pattern validacao = padraoValidacaoInput.matcher(opcao).pattern();
+
+            System.out.println(validacao);
+
+                switch (Integer.parseInt(opcao)) {
+                    case 0:
+                        System.out.println("Opção não implementada, por favor, tente novamente");
+                        System.out.println(menuPrincipal);
+                        break;
+                    case 1:
+                        System.out.println("Opção não implementada, por favor, tente novamente");
+                        System.out.println(menuPrincipal);
+                        break;
+                    case 2:
+                        System.out.println("Opção não implementada, por favor, tente novamente");
+                        System.out.println(menuPrincipal);
+                        break;
+                    case 3:
+                        System.out.printf("""
+                                ==========================================
+                                Obrigado por usar Questionarios Incriveis!
+                                ==========================================
+                                """);
+                        loopingMenu = false;
+                        sc.close();
+                        break;
+                    default:
+                        System.out.println("Opção não suportada, por favor, selecione uma opção do menu principal.\n");
+                        System.out.println(menuPrincipal);
+                        break;
+                }
             }
     }
 } 
