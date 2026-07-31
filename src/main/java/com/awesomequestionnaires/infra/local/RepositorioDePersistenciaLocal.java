a762819fbdd313861b869a9a51961e5d47889d81
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-07-31 11:43:45 -0300

    feat: create questionnaire file

--- src/main/java/com/awesomequestionnaires/infra/local/RepositorioDePersistenciaLocal.java
+++ src/main/java/com/awesomequestionnaires/infra/local/RepositorioDePersistenciaLocal.java
@@ -1,5 +1,6 @@
 package com.awesomequestionnaires.infra.local;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,10 +12,15 @@ public class RepositorioDePersistenciaLocal {
         String nomePasta = "questionarios";
         Path raiz = Path.of("").toAbsolutePath();
         Path diretorioQuestionarios = raiz.resolve(nomePasta);
+        // System.out.println(diretorioQuestionarios);
         Files.createDirectories(diretorioQuestionarios);
 
         String nomeQuestionario = "timestamp_" + UUID.randomUUID() + ".json";
-        System.out.println(nomeQuestionario);
+        // System.out.println(nomeQuestionario);
+        // Path caminhoArquivoQuestionario = diretorioQuestionarios.resolve(nomeQuestionario);
+        // System.out.println(caminhoArquivoQuestionario);
+        FileWriter arquivoQuestionario = new FileWriter(nomeQuestionario);
+        System.out.println(arquivoQuestionario);
 
         return "WIP - criaArquivoQuestionario";
     }
