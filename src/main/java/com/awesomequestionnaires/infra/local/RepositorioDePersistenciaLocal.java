8be6cdcd76d88e5bc1275c9858060f87812c39b1
renatarsilvatw <renata.silva@thoughtworks.com>
2026-08-03 17:03:32 -0300

    refactor: modifica o retorno dos metodos para Path

--- src/main/java/com/awesomequestionnaires/infra/local/RepositorioDePersistenciaLocal.java
+++ src/main/java/com/awesomequestionnaires/infra/local/RepositorioDePersistenciaLocal.java
@@ -7,12 +7,12 @@ import java.nio.file.Path;
 import java.util.UUID;
 
 public class RepositorioDePersistenciaLocal {
-    public static void criarDiretorioArquivoQuestionario() throws IOException {
+    public static Path criarDiretorioArquivoQuestionario() throws IOException {
         String nomePasta = "questionarios";
         Path diretorioQuestionario = RepositorioDePersistenciaLocal.criaDiretorioQuestionario(nomePasta);
         long timestamp = System.currentTimeMillis();
         String nomeQuestionario = timestamp + "_" + UUID.randomUUID() + ".json";
-        RepositorioDePersistenciaLocal.criaArquivoQuestionario(diretorioQuestionario, nomeQuestionario);
+        return RepositorioDePersistenciaLocal.criaArquivoQuestionario(diretorioQuestionario, nomeQuestionario);
     }
 
     private static Path criaDiretorioQuestionario(String nomeDiretorio) throws IOException {
@@ -22,10 +22,11 @@ public class RepositorioDePersistenciaLocal {
         return diretorioQuestionarios;
     }
 
-    private static void criaArquivoQuestionario(Path caminhoDiretorioQuestionarios, String nomeArquivoQuestionarios) throws IOException {
+    private static Path criaArquivoQuestionario(Path caminhoDiretorioQuestionarios, String nomeArquivoQuestionarios) throws IOException {
         Path caminhoArquivoQuestionario = caminhoDiretorioQuestionarios.resolve(nomeArquivoQuestionarios);
         FileWriter arquivoQuestionario = new FileWriter(caminhoArquivoQuestionario.toString());
         arquivoQuestionario.write("{}");
         arquivoQuestionario.close();
+        return caminhoArquivoQuestionario;
     }
 }
