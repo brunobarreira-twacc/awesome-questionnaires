5fc8b38534decccf184acb8c5e47b4b1f0e89f15
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-04 15:50:59 -0300

    feat: add ExecutionSteps enumerator

---
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/ExecutionSteps.java
@@ -0,0 +1,9 @@
+package com.awesomequestionnaires.commandLineInterface;
+
+public enum ExecutionSteps {
+    MAIN_MENU,
+    CREATE_QUESTIONNAIRE,
+    MANAGE_QUESTIONNAIRE,
+    RESPOND_QUESTIONNAIRE,
+    EXIT
+}
