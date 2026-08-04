c29372c81e3d64cf8caef11e942f8270b916bfc6
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-04 15:50:35 -0300

    feat: implements Boot class

--- src/main/java/com/awesomequestionnaires/commandLineInterface/Boot.java
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/Boot.java
@@ -1,5 +1,9 @@
 package com.awesomequestionnaires.commandLineInterface;
 
+import java.util.Map;
+
+import com.awesomequestionnaires.commandLineInterface.actions.CreateQuestionnaire;
+
 public class Boot {
     
     private String mainMenu = "====================================\nSaudacao do Questionarios Incriveis.\n====================================\n" + 
@@ -10,6 +14,28 @@ public class Boot {
                                 "Por favor, digite sua opcao\n\n";
 
     public void execute(CliContext context) {
-      context.ask(mainMenu);
+
+      boolean loopingMenu = true;
+
+      Map<ExecutionSteps, MenuAction> actions = Map.of(
+        ExecutionSteps.CREATE_QUESTIONNAIRE, new CreateQuestionnaire()
+      );
+
+      Map<String, ExecutionSteps> menuOptions = Map.of(
+        "0", ExecutionSteps.CREATE_QUESTIONNAIRE,
+        "1", ExecutionSteps.MANAGE_QUESTIONNAIRE,
+        "2", ExecutionSteps.RESPOND_QUESTIONNAIRE,
+        "3", ExecutionSteps.EXIT
+      );
+
+      while(loopingMenu) {
+        String userOption = context.ask(mainMenu);
+
+        ExecutionSteps nextStep = menuOptions.getOrDefault(userOption, ExecutionSteps.MAIN_MENU); // CREATE_QUESTIONNAIRE
+
+        MenuAction action = actions.get(nextStep);
+
+        action.execute(context);
+        }
     }
 }
