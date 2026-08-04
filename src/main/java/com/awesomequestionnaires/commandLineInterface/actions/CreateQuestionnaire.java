32d600d50530a8cae661b60f51dd0d2f40f03a34
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-04 15:51:38 -0300

    feat: add CreateQuestionnaire action class

---
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestionnaire.java
@@ -0,0 +1,14 @@
+package com.awesomequestionnaires.commandLineInterface.actions;
+
+import com.awesomequestionnaires.commandLineInterface.CliContext;
+import com.awesomequestionnaires.commandLineInterface.ExecutionSteps;
+import com.awesomequestionnaires.commandLineInterface.MenuAction;
+
+public class CreateQuestionnaire implements MenuAction {
+
+    @Override
+    public ExecutionSteps execute(CliContext context) {
+        System.out.println("keep calm and carry on renata and bruno");
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+}
