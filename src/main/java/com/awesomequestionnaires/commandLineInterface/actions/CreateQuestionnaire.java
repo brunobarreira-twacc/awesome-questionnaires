20c0615b20dd3778da4814a679bbf47df6df997d
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-07 16:28:10 -0300

    refactor: move questionnaire status and use save method from repository

--- src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestionnaire.java
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestionnaire.java
@@ -7,17 +7,41 @@ import com.awesomequestionnaires.commandLineInterface.CliContext;
 import com.awesomequestionnaires.commandLineInterface.ExecutionSteps;
 import com.awesomequestionnaires.commandLineInterface.MenuAction;
 import com.awesomequestionnaires.domain.Questionnaire;
+import com.awesomequestionnaires.domain.QuestionnaireStatus;
 import com.awesomequestionnaires.infra.local.RepositorioDePersistenciaLocal;
 
 public class CreateQuestionnaire implements MenuAction {
 
     private String[] criacaoDoQuestionario = {
         "1 - Digite o nome do questionario:",
-        "2 - Descrição do questionario:"
+        "2 - Descrição do questionario:",
+        "4 - Publicar agora? Sim/Nao"
     };
 
+    private static Boolean validarInputStatusQuestionario(String statusQuestionario) {
+        if (!"sim".equalsIgnoreCase(statusQuestionario) && !"não".equalsIgnoreCase(statusQuestionario)){
+            return false;
+        }
+        return true;
+    }
+
+    private  void preencherStatusQuestionario(String inputUsuario, Questionnaire questionnaire) {
+        Boolean inputValido = validarInputStatusQuestionario(inputUsuario);
+        if(inputValido) {
+            if (inputUsuario.equals("sim") || inputUsuario.equals("SIM")){
+                questionnaire.setStatusQuestionario(QuestionnaireStatus.PUBLISHED);
+            }
+
+            if (inputUsuario.equals("não") || inputUsuario.equals("NÃO")){
+                questionnaire.setStatusQuestionario(QuestionnaireStatus.DRAFT);
+            }
+        } else {
+            System.out.println("Erro: Input inválido, por favor, digite Sim ou Não");
+        }
+    }
+
     @Override
-    public ExecutionSteps execute(CliContext context) throws IOException {
+    public ExecutionSteps execute(CliContext context) throws IOException  {
         Path arquivoquestionario = RepositorioDePersistenciaLocal.criarDiretorioArquivoQuestionario();
 
         Questionnaire novoQuestionario = new Questionnaire();
@@ -28,9 +52,20 @@ public class CreateQuestionnaire implements MenuAction {
 
         String descricaoQuestionario = context.ask(criacaoDoQuestionario[1]);
         novoQuestionario.setDescricaoQuestionario(descricaoQuestionario);
+        
+        Boolean ligado = true;
+        while (ligado) {
+            String statusQuestionario = context.ask(criacaoDoQuestionario[2]);
+            preencherStatusQuestionario(statusQuestionario, novoQuestionario); // ocorre validacao
+            if(novoQuestionario.getStatusQuestionario() != null) {
+                ligado = false;
+            }
+        }
 
         System.out.println(novoQuestionario.getNomeQuestionario() + " " + novoQuestionario.getDescricaoQuestionario() + " " + novoQuestionario.getStatusQuestionario());
-        
+
+        RepositorioDePersistenciaLocal.save(arquivoquestionario.toFile(), novoQuestionario);
+
         return ExecutionSteps.MAIN_MENU;
     }
 }
