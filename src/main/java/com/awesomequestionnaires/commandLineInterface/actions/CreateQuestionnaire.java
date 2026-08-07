c0f4e9f9f2c75b6031aeab1de596c08356c65536
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-07 15:35:22 -0300

    refactor: create questionnaire inside action

--- src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestionnaire.java
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestionnaire.java
@@ -1,14 +1,36 @@
 package com.awesomequestionnaires.commandLineInterface.actions;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import com.awesomequestionnaires.commandLineInterface.CliContext;
 import com.awesomequestionnaires.commandLineInterface.ExecutionSteps;
 import com.awesomequestionnaires.commandLineInterface.MenuAction;
+import com.awesomequestionnaires.domain.Questionnaire;
+import com.awesomequestionnaires.infra.local.RepositorioDePersistenciaLocal;
 
 public class CreateQuestionnaire implements MenuAction {
 
+    private String[] criacaoDoQuestionario = {
+        "1 - Digite o nome do questionario:",
+        "2 - Descrição do questionario:"
+    };
+
     @Override
-    public ExecutionSteps execute(CliContext context) {
-        System.out.println("keep calm and carry on renata and bruno");
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    public ExecutionSteps execute(CliContext context) throws IOException {
+        Path arquivoquestionario = RepositorioDePersistenciaLocal.criarDiretorioArquivoQuestionario();
+
+        Questionnaire novoQuestionario = new Questionnaire();
+        // loopingMenu = false;
+        
+        String nomeQuestionario = context.ask(criacaoDoQuestionario[0]);
+        novoQuestionario.setNomeQuestionario(nomeQuestionario);
+
+        String descricaoQuestionario = context.ask(criacaoDoQuestionario[1]);
+        novoQuestionario.setDescricaoQuestionario(descricaoQuestionario);
+
+        System.out.println(novoQuestionario.getNomeQuestionario() + " " + novoQuestionario.getDescricaoQuestionario() + " " + novoQuestionario.getStatusQuestionario());
+        
+        return ExecutionSteps.MAIN_MENU;
     }
 }
