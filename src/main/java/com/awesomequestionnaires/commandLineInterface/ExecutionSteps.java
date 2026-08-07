4ebfa5871d42b972602a27da71c9bfc3c45356a6
brunobarreira-twacc <bruno.barreira@thoughtworks.com>
2026-08-07 16:35:36 -0300

    feat: register new action create question

--- src/main/java/com/awesomequestionnaires/commandLineInterface/ExecutionSteps.java
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/ExecutionSteps.java
@@ -3,6 +3,7 @@ package com.awesomequestionnaires.commandLineInterface;
 public enum ExecutionSteps {
     MAIN_MENU,
     CREATE_QUESTIONNAIRE,
+    CREATE_QUESTION,
     MANAGE_QUESTIONNAIRE,
     RESPOND_QUESTIONNAIRE,
     EXIT
