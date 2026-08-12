6f2bccf6f89a4b87e02501712fba433216b473d4
renatarsilvatw <renata.silva@thoughtworks.com>
2026-08-12 16:11:42 -0300

    feat: cria classe CreateQuestion

--- src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestion.java
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/actions/CreateQuestion.java
@@ -1,21 +1,33 @@
 package com.awesomequestionnaires.commandLineInterface.actions;
 
 import java.io.IOException;
+import java.util.Map;
 
 import com.awesomequestionnaires.commandLineInterface.CliContext;
 import com.awesomequestionnaires.commandLineInterface.ExecutionSteps;
 import com.awesomequestionnaires.commandLineInterface.MenuAction;
+import com.awesomequestionnaires.domain.Question;
 
 public class CreateQuestion implements MenuAction {
 
     private String[] createQuestionMessages = {
-        "Questionário criado com sucesso, agora vamos criar uma pergunta.\n1 - Digite o texto que será exibido para a pessoa usuária que vai responder seu questionário. Esse texto deve obrigatoriamente ser uma pergunta:"
+        "Questionário criado com sucesso, agora vamos criar uma pergunta.\n1 - Digite o texto que será exibido para a pessoa usuária que vai responder seu questionário. Esse texto deve obrigatoriamente ser uma pergunta:",
+            "2 - Escolha um tipo de questão:\n 1 - Opção única, usuário podem selecionar apenas uma opção para essa questão.\n 2 - Múltiplas opções, usuário podem selecionar uma ou mais opções para essa questão. \n 3 - Condicional, dada uma resposta condicional, questões adicionais aparecem para o usuário responder."
     };
 
     @Override
     public ExecutionSteps execute(CliContext context) throws IOException {
-        context.ask(createQuestionMessages[0]);
+        String textoQuestao = context.ask(createQuestionMessages[0]);
+        Question questionType = new Question();
+        questionType.setDisplayText(textoQuestao);
 
-        return ExecutionSteps.MAIN_MENU;
+        String tipoQuestao = context.ask(createQuestionMessages[1]);
+
+        Map<String, ExecutionSteps> questionTypeOption = Map.of(
+                "1", ExecutionSteps.SINGLE_OPTION_QUESTION
+        );
+
+        return questionTypeOption.getOrDefault(tipoQuestao, ExecutionSteps.MAIN_MENU);
+
     }
 }
