20811f129b9550428db1959fdd279cf0ceb7c1ea
renatarsilvatw <renata.silva@thoughtworks.com>
2026-08-12 16:14:27 -0300

    feat: adiciona classe de acao SingleOptionQuestion

---
+++ src/main/java/com/awesomequestionnaires/commandLineInterface/actions/SingleOptionQuestion.java
@@ -0,0 +1,15 @@
+package com.awesomequestionnaires.commandLineInterface.actions;
+
+import com.awesomequestionnaires.commandLineInterface.CliContext;
+import com.awesomequestionnaires.commandLineInterface.ExecutionSteps;
+import com.awesomequestionnaires.commandLineInterface.MenuAction;
+
+import java.io.IOException;
+
+public class SingleOptionQuestion implements MenuAction {
+
+    @Override
+    public ExecutionSteps execute(CliContext context) throws IOException {
+        return null;
+    }
+}
